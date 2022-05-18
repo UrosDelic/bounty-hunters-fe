64168f2a94d82a80626671fbe7b824cb1f579003
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-05-18 23:54:01 +0200

    [vuk] Styllize header a bit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import { Flex, Heading } from '@chakra-ui/react';
 
 function Header() {
   return (
-    <Flex minH="8vh" alignItems="center" backgroundColor="black" color="white">
-      <Heading as="h3">This is a header</Heading>
+    <Flex
+      minH="8vh"
+      alignItems="center"
+      backgroundColor="black"
+      color="white"
+      padding="10px 15px"
+      justifyContent="space-between"
+    >
+      <Heading as="h3">Quantox</Heading>
     </Flex>
   );
 }
