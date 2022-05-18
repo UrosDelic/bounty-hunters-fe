40f1c589d2149d227c80a27020d22b097701f912
Uros47 <urosdelic89@gmail.com>
2022-05-18 14:28:57 +0200

    browser router moved to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { BrowserRouter } from 'react-router-dom';
+import { ColorModeScript } from '@chakra-ui/react';
 
 function App() {
   return (
     <ChakraProvider>
-      <Routes>
-        <Route exact path="/" element={<div>test</div>}></Route>
-      </Routes>
+      <ColorModeScript />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <div>
+                test
+                <ColorModeSwitcher></ColorModeSwitcher>
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-// import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      {/* <ColorModeScript /> */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
