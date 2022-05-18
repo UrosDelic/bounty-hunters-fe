d08bae0145c9da9d11ad9d1b7a9dc5dbda267711
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-05-18 17:23:19 +0200

    [vuk] Add Layout component

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { BrowserRouter } from 'react-router-dom';
+import { ColorModeScript } from '@chakra-ui/react';
+import Layout from './components/Layout.jsx';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <ColorModeScript />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route
+              path="/"
+              element={
+                <div>
+                  test
+                  <ColorModeSwitcher></ColorModeSwitcher>
+                </div>
+              }
+            />
+            <Route path="/feed" element={<div>feed</div>} />
+            <Route path="/new-tasks" element={<div>new tasks</div>} />
+            <Route path="/my-tasks" element={<div>my tasks</div>} />
+            <Route path="/wallet" element={<div>wallet</div>} />
+            <Route path="/store" element={<div>store</div>} />
+            <Route path="/my-orders" element={<div>my orders</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/ui-data/LayoutData.js
@@ -0,0 +1,34 @@
+const layoutData = [
+  {
+    role: 'employee',
+    text: 'feed',
+    route: '/feed',
+  },
+  {
+    role: 'employee',
+    text: 'new tasks',
+    route: '/new-tasks',
+  },
+  {
+    role: 'employee',
+    text: 'my tasks',
+    route: '/my-tasks',
+  },
+  {
+    role: 'employee',
+    text: 'wallet',
+    route: '/wallet',
+  },
+  {
+    role: 'employee',
+    text: 'store',
+    route: '/store',
+  },
+  {
+    role: 'employee',
+    text: 'my orders',
+    route: '/my-orders',
+  },
+];
+
+export default layoutData;
