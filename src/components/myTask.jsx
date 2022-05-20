b6a753653415e5ace071d3c2cfda7da4b3814483
Uros47 <urosdelic89@gmail.com>
2022-05-20 15:50:15 +0200

    colors

--- src/components/myTask.jsx
+++ src/components/myTask.jsx
@@ -3,7 +3,7 @@ import { Box, Badge } from '@chakra-ui/react';
 const MyTask = () => {
   return (
     <Box
-      bg="#B4B8C5"
+      bg="#E9EBF8"
       height="300px"
       marginTop="50px"
       margin="10px"
@@ -12,7 +12,7 @@ const MyTask = () => {
     >
       <Box textAlign="center">My Task headline</Box>
       <Box ml={1}>
-        <Badge borderRadius="full" px="5" bg="#e9ebf8">
+        <Badge borderRadius="full" px="5" bg="#B4B8C5">
           To Do
         </Badge>
       </Box>
