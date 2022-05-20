441b85f05deb229cf718f0744345efda1dd186a0
Uros47 <urosdelic89@gmail.com>
2022-05-20 15:47:38 +0200

    myTask component

--- src/components/myTask.jsx
+++ src/components/myTask.jsx
@@ -1,5 +1,24 @@
+import { Box, Badge } from '@chakra-ui/react';
+
 const MyTask = () => {
-  return <div>test my task</div>;
+  return (
+    <Box
+      bg="#B4B8C5"
+      height="300px"
+      marginTop="50px"
+      margin="10px"
+      width="400px"
+      borderRadius="lg"
+    >
+      <Box textAlign="center">My Task headline</Box>
+      <Box ml={1}>
+        <Badge borderRadius="full" px="5" bg="#e9ebf8">
+          To Do
+        </Badge>
+      </Box>
+      <Box textAlign="center">Task description</Box>
+    </Box>
+  );
 };
 
 export default MyTask;
--- src/pages/MyTasks.jsx
+++ src/pages/MyTasks.jsx
@@ -1,10 +1,12 @@
 import MyTask from '../components/myTask';
-
+import { Box } from '@chakra-ui/react';
 const MyTasksPage = () => {
   return (
-    <>
+    <Box display="flex" flexDirection="row">
       <MyTask />
-    </>
+      <MyTask />
+      <MyTask />
+    </Box>
   );
 };
 
