d8e4fc95607d5c1baaf5efadf21d12cffc2a362d
Uros47 <urosdelic89@gmail.com>
2022-05-25 16:08:10 +0200

    link

--- src/pages/my-tasks/MyTasks.jsx
+++ src/pages/my-tasks/MyTasks.jsx
@@ -2,7 +2,7 @@ import MyTask from '../../components/my-tasks/myTask';
 import { Box } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import TasksService from '../../services/tasks';
-
+import { Link } from 'react-router-dom';
 const tasks = [
   { id: 1, headline: 'task 1 headline', txt: 'task 1 text', status: 'To Do' },
   { id: 2, headline: 'task 2 headline', txt: 'task 2 text', status: 'Done' },
@@ -17,12 +17,14 @@ const MyTasksPage = () => {
   return (
     <Box display="flex" flexDirection="row">
       {tasks.map(task => (
-        <MyTask
-          key={task.id}
-          headline={task.headline}
-          text={task.txt}
-          status={task.status}
-        ></MyTask>
+        <Link to={`/task-details/${task.id}`}>
+          <MyTask
+            key={task.id}
+            headline={task.headline}
+            text={task.txt}
+            status={task.status}
+          ></MyTask>
+        </Link>
       ))}
     </Box>
   );
