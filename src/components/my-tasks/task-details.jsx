cab95227b0a695946eb0e45e1059185f470daade
Uros Delic <uros.delic@quantox.com>
2022-05-26 13:44:38 +0200

    td

--- src/components/my-tasks/task-details.jsx
+++ src/components/my-tasks/task-details.jsx
@@ -1,7 +1,11 @@
+import { Box, Container } from '@chakra-ui/react';
 import React from 'react';
 
 const TaskDetails = () => {
-  return <div>Task details</div>;
+  return <Container className='test-details' width='50vw' height='50vh' bg='yellow' color='yellow' marginTop='50px'>
+    <Box>
+    </Box>
+  </Container>
 };
 
 export default TaskDetails;
