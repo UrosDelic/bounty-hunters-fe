ffecbe01327d1302d8ac2e4064988e9fbcce0706
vukstojanovic <vuks838@gmail.com>
2022-05-27 13:07:15 +0200

    [fix-proteted-routes] Add condition for the case when user is logged out

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -5,10 +5,14 @@ function ProtectedRoute({ allowedRoles }) {
   const { userRole } = useAppContext();
   const location = useLocation();
 
+  if (!userRole) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
   if (allowedRoles.includes(userRole)) {
     return <Outlet />;
   } else {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/" state={{ from: location }} replace />;
   }
 }
 
