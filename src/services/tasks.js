dc24c7a99d63c2f74a40857f358415f7814026cd
Uros Delic <uros.delic@quantox.com>
2022-05-31 12:53:18 +0200

    test

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,13 +1,3 @@
-import { initHttp } from '../http';
-
-class TasksService {
-  constructor() {
-    this.http = initHttp();
-  }
-
-  getTasks() {
-    return this.http.get('api/users?page=2');
-  }
+function test(){
+    return 'nothing';
 }
-
-export default TasksService;
