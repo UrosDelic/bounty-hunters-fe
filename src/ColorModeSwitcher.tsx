674a0540061b1a286d36a471a0338ac05fe5fb45
Uros Delic <uros.delic@quantox.com>
2022-06-02 21:28:20 +0200

    color mode switcher polish

--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -7,7 +7,9 @@ import {
 } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-export const ColorModeSwitcher: React.FC<Partial<IconButtonProps>> = props => {
+type ColorModeSwitcherProps = Omit<IconButtonProps, 'aria-label'>;
+
+export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
