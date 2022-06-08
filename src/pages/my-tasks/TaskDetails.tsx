887120d341fa25749df80fd3c11b1d1852f852b3
Uros Delic <uros.delic@quantox.com>
2022-06-08 19:06:43 +0200

    store imported to details

--- src/pages/my-tasks/TaskDetails.tsx
+++ src/pages/my-tasks/TaskDetails.tsx
@@ -1,15 +1,14 @@
 import TaskDetails from '../../components/my-tasks/TaskDetails';
 import { useParams } from 'react-router-dom';
-import TasksService from '../../services/tasks';
 import { useEffect } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
+import TasksStore from '../../stores/tasks';
 
 const TaskDetailsPage = () => {
-  const service = new TasksService();
   const { id } = useParams();
   useEffect(() => {
     if (id) {
-      service.getTasksById(id);
+      TasksStore.getTaskDetailsById(id);
     }
   });
   return (
