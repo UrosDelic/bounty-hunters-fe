ff396f968728a55a776c488a6676ce77810d5021
Uros Delic <uros.delic@quantox.com>
2022-06-14 19:23:51 +0200

    html cleanup

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,12 +1,4 @@
-import {
-  Container,
-  Heading,
-  Stack,
-  useBreakpointValue,
-  Button,
-  Box,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Container, Heading, Stack, Box } from '@chakra-ui/react';
 import {
   GoogleLogin,
   GoogleLoginResponse,
@@ -26,34 +18,20 @@ const Login = () => {
       py={{ base: '12', md: '24' }}
       px={{ base: '0', sm: '8' }}
     >
-      <Stack spacing="8">
-        <Stack spacing="6">
-          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-            <Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>
-              Log in to your Bounty Hunters account
-            </Heading>
-          </Stack>
-        </Stack>
-        <Box
-          py={{ base: '0', sm: '8' }}
-          px={{ base: '4', sm: '10' }}
-          bg={useBreakpointValue({ base: 'transparent', sm: 'bg-surface' })}
-          boxShadow={{ base: 'none', sm: useColorModeValue('md', 'md-dark') }}
-          borderRadius={{ base: 'none', sm: 'xl' }}
-        >
-          <Stack spacing="6">
-            <Stack spacing="6">
-              <GoogleLogin
-                clientId="267868351623-8an5obdlb5j0s2n0dopo0ntp7g7ip3sv.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-              />
-            </Stack>
-          </Stack>
-        </Box>
+      <Stack textAlign="center">
+        <Heading size="md">Log in to your Bounty Hunters account</Heading>
       </Stack>
+      <Box py={{ base: '0', sm: '8' }} px={{ base: '4', sm: '10' }}>
+        <Stack>
+          <GoogleLogin
+            clientId="267868351623-8an5obdlb5j0s2n0dopo0ntp7g7ip3sv.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </Stack>
+      </Box>
     </Container>
   );
 };
