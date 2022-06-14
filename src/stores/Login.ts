27daafce6bb520d74e8802363c4ae181404621d5
Uros Delic <uros.delic@quantox.com>
2022-06-14 19:55:41 +0200

    removed login method from store

--- src/stores/Login.ts
+++ src/stores/Login.ts
@@ -2,18 +2,10 @@ import { initHttp } from 'http/index';
 import { makeAutoObservable, runInAction } from 'mobx';
 
 class LoginStore {
+  _userData = {};
   constructor(private http = initHttp()) {
     makeAutoObservable(this);
   }
-
-  login = async (credentials: {}) => {
-    const { data } = await this.http.post(`/auth/login/google`, credentials);
-    runInAction(() => {
-      if (data) {
-        console.log(data, 'data iz login');
-      }
-    });
-  };
 }
 
 export default new LoginStore();
