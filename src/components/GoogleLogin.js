425483c7aad7b87734c59f01aafae8ce5e4c16d7
Uros Delic <uros.delic@quantox.com>
2022-06-20 10:24:29 +0200

    client id fix

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import LoginStore from 'stores/Login';
 
+const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 const GoogleLogin = () => {
   const handleResponse = response => {
     LoginStore.login(response);
@@ -11,14 +13,13 @@ const GoogleLogin = () => {
 
   useEffect(() => {
     google.accounts.id.initialize({
-      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      client_id: client_id,
       callback: handleResponse,
     });
     google.accounts.id.renderButton(document.getElementById('google-btn'), {
       shape: 'pill',
       size: 'large',
       text: 'Signin with google',
-      ux_mode: 'popup',
       width: '250px',
     });
   }, []);
